ffe0dd33212e1642d7be3dffc99727f8987ac211
405 <405@academicos.cesde.edu.co>
2022-12-13 11:40:35 -0500

    a

--- src/AmpliarInfo/AmpliarInfo.js
+++ src/AmpliarInfo/AmpliarInfo.js
@@ -1,4 +1,4 @@
-
+import './AmpliarInfo.css'
 import { useLocation } from "react-router-dom"
 
 export function AmpliarInfo(){
@@ -10,7 +10,34 @@ export function AmpliarInfo(){
 
     return(
         <>
-        <h1>{producto.nombre}</h1>
+            <div className="container">
+                <div className="row my-5">
+                    <div className="col-12 col-md-5 ">                      
+                        <img src={producto.foto} alt="foto" className="img-fluid w-100"/>
+                    </div>
+                    <div className="col-12 col-md-4 border p-3">
+                        <h2 className="fw-bold mark">{producto.nombre}</h2>
+                        <h1 className="mt-3 display">{producto.precio}    <span className="descuento">20% off</span></h1>
+                        <br></br>
+                        <span class="badge text-bg-warning">descuento solo x hoy</span>
+                        <p className='fw-bold'>hasta 48 cuotas</p>
+                        <img src="https://firebasestorage.googleapis.com/v0/b/spotify-alcolirykoz-a8b7c.appspot.com/o/visa%20(1).png?alt=media&token=bb6ded2c-4717-4293-b743-561621c30f05" alt='visa' className='img-fluid '/>
+                        <img src="https://firebasestorage.googleapis.com/v0/b/spotify-alcolirykoz-a8b7c.appspot.com/o/american-express%20(1).png?alt=media&token=d90eef2e-9881-44a2-86e0-1c6e8365d53c" alt='america express' className='img-fluid ms-1'/>
+                        <br></br>
+
+                        <i class="bi bi-truck mt-5 fs-1 ">
+                            <span className="text-success text-bold fuente " > Envio gratis a nivel nacional</span></i>
+                        <p className='fw-bold'>Conoce los tiempos y las formas de envio</p>
+
+                        <i class="bi bi-arrow-clockwise devolucion "><span className="text-success text-bold fuente "> Devolucion gratis</span></i>
+                        <p className='fw-bold'>30 dias para reembolsos</p>
+
+                        <h4 className='fw-bold'>DESCRIPCION</h4>
+                        <h2 className='descripcion'>{producto.descripcion}</h2>
+
+                    </div>
+                </div>
+            </div>
         </>
     )
         
--- src/Mercancia/Mercancia.js
+++ src/Mercancia/Mercancia.js
@@ -23,32 +23,38 @@ export function Mercancia (){
         {
             nombre:"BOLSO ORIGINAL",
             foto:"https://firebasestorage.googleapis.com/v0/b/spotify-alcolirykoz-a8b7c.appspot.com/o/bolso.jpg?alt=media&token=9624ae53-b03e-402b-9571-c0e21c93c768",
-            precio:"$ 300.000"
+            precio:"$ 300.000",
+            descripcion:""
         },
         {
             nombre:"BUSO ORIGINAL",
             foto:"https://firebasestorage.googleapis.com/v0/b/spotify-alcolirykoz-a8b7c.appspot.com/o/busos.jpg?alt=media&token=609db8b3-e592-4672-adfe-fe81985c0e17",
-            precio:"$ 180.000"
+            precio:"$ 180.000",
+            descripcion:""
         },
         {
             nombre:"CAMISA TRIPLE A",
             foto:"https://firebasestorage.googleapis.com/v0/b/spotify-alcolirykoz-a8b7c.appspot.com/o/camisas.jpg?alt=media&token=0bee1f10-9f6d-4bda-99fe-ee160b3e6ed7",
-            precio:"$ 150.000"
+            precio:"$ 150.000",
+            descripcion:""
         },
         {
             nombre:"CD PREMIUM",
             foto:"https://firebasestorage.googleapis.com/v0/b/spotify-alcolirykoz-a8b7c.appspot.com/o/disc.jpg?alt=media&token=11e2268c-8e71-47a6-9d7f-0b16dde19763",
-            precio:"$ 80.000"
+            precio:"$ 80.000",
+            descripcion:""
         },
         {
             nombre:"FORRO CEL",
             foto:"https://firebasestorage.googleapis.com/v0/b/spotify-alcolirykoz-a8b7c.appspot.com/o/forro.webp?alt=media&token=cf7246b2-bd18-4cb3-b0a4-9a5aed132511",
-            precio:"$ 50.000"
+            precio:"$ 50.000",
+            descripcion:""
         },
         {
             nombre:"POSTER",
             foto:"https://firebasestorage.googleapis.com/v0/b/spotify-alcolirykoz-a8b7c.appspot.com/o/poster.jpg?alt=media&token=c868166b-b31f-436f-9747-5f3672439689",
-            precio:"$ 55.000"
+            precio:"$ 55.000",
+            descripcion:""
         },
 
     ]
@@ -66,9 +72,9 @@ export function Mercancia (){
                                 <div className="col">
                                     <div className="card h-100 shadow">
                                         
-                                        <h1 className="text-center mt-1 mark">{producto.nombre}</h1>
+                                        <h1 className="text-center mt-1 ">{producto.nombre}</h1>
                                         <img src={producto.foto} alt="foto" className="h-100 img-fluid w-100 mt-2 "/>
-                                        <h1 className="text-center mt-1">{producto.precio}</h1>
+                                        <h1 className="text-center mt-1 mark">{producto.precio}</h1>
 
                                         <button className="btn btn-primary mx-5 my-3" onClick={
                                             function(){
